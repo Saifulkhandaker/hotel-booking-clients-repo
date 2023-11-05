3a8be4dfecc1d98c7931e3130172436cb0b921c6
Saifulkhandaker <khsaiful67@gmail.com>
2023-11-05 11:53:26 +0600

    primary setup complete

---
+++ src/firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYXc1SnrGackv64r_us9sVvdFaFsRR_DA",
+  authDomain: "hotel-boo-c51b7.firebaseapp.com",
+  projectId: "hotel-boo-c51b7",
+  storageBucket: "hotel-boo-c51b7.appspot.com",
+  messagingSenderId: "253429984203",
+  appId: "1:253429984203:web:9270eaf10999a5d078e44e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

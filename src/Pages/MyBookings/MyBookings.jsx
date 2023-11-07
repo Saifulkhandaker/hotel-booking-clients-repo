a0b166474463a6443401d714a350f4ee95ebea22
Saifulkhandaker <khsaiful67@gmail.com>
2023-11-07 22:02:13 +0600

    delete button added in room details table

--- src/Pages/MyBookings/MyBookings.jsx
+++ src/Pages/MyBookings/MyBookings.jsx
@@ -5,19 +5,53 @@ import { AuthContext } from "../../Providers/AuthProvider";
 import { useEffect } from "react";
 import { useState } from "react";
 import BookingTable from "./BookingTable";
+import Swal from "sweetalert2";
 
 const MyBookings = () => {
+  const { user } = useContext(AuthContext);
+  const [bookings, setBookings] = useState([]);
 
+  const url = `http://localhost:5000/bookings?email=${user?.email}`;
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setBookings(data));
+  }, []);
 
-    const {user} = useContext(AuthContext)
-    const [bookings, setBookings] = useState([]);
-
-    const url = `http://localhost:5000/bookings?email=${user?.email}`;
-    useEffect(() => {
-        fetch(url)
-        .then(res => res.json())
-        .then(data => setBookings(data))
-    }, [])
+  // handle delete
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are You Sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/bookings/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Success",
+                text: "Room Booked Successfully",
+                icon: "success",
+                confirmButtonText: "Cool",
+              });
+              const remaining = bookings.filter(
+                (booking) => booking._id !== id
+              );
+              setBookings(remaining);
+            }
+          });
+      }
+    });
+  };
 
   return (
     <div>
@@ -32,39 +66,38 @@ const MyBookings = () => {
           </Link>
         </h1>
       </div>
-      <div >
-        <h1 className="text-2xl md:text-5xl font-medium text-center mt-5 mb-10 ">Tottal_Booked: {bookings.length}</h1>
+      <div>
+        <h1 className="text-2xl md:text-5xl font-medium text-center mt-5 mb-10 ">
+          Tottal_Booked: {bookings.length}
+        </h1>
         {/* booked item detail */}
         <div className="overflow-x-auto">
-  <table className="table w-11/12 mx-auto">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>Delete</th>
-        <th>Img</th>
-        <th>Room Name</th>
-        <th>Check In</th>
-        <th>Check Out</th>
-        <th>Update</th>
-      </tr>
-    </thead>
-    <tbody>
-      
-      {
-        bookings.map(booking => <BookingTable
-        key={booking._id}
-        booking={booking}
-        ></BookingTable>)
-      }
-      
-    </tbody>
-
-    
-  </table>
-</div>
+          <table className="table w-11/12 mx-auto">
+            {bookings.length > 0 && (
+              <thead>
+                <tr>
+                  <th>Delete</th>
+                  <th>Img</th>
+                  <th>Room Name</th>
+                  <th>Check In</th>
+                  <th>Check Out</th>
+                  <th>Update</th>
+                </tr>
+              </thead>
+            )}
+            <tbody>
+              {bookings.map((booking) => (
+                <BookingTable
+                  key={booking._id}
+                  booking={booking}
+                  handleDelete={handleDelete}
+                ></BookingTable>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
 };
-
 export default MyBookings;
